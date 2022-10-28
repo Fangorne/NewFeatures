7bdad83a156d4b3f9f4e21696cee4fd3cc9fb8e3
marc.ranchin <marc.ranchin@mylittlefintech.com>
2022-10-28 09:51:52 +0200

    Add project files.

---
+++ SpanDemo/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BenchmarkDotNet.Running;
+using SpanDemo;
+
+var summary = BenchmarkRunner.Run<SpanVsString>();
+Console.ReadLine();
+
+Console.WriteLine("Hello, World!");
---
+++ SpanDemo/SpanVsString.cs
@@ -0,0 +1,71 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace SpanDemo;
+
+public class SpanVsString
+{
+    #region fields
+
+    private readonly string _hamletText;
+
+    #endregion
+
+    #region constructors
+
+    public SpanVsString()
+    {
+        using StreamReader streamReader = File.OpenText("le-petit-prince--antoine-de-saint-exupery.txt");
+        _hamletText = streamReader.ReadToEnd();
+    }
+
+    #endregion
+
+    #region
+
+    [Benchmark]
+    public void ParseWithString()
+    {
+        var indexPrev = 0;
+        var indexCurrent = 0;
+        var rowNum = 0;
+        foreach (var c in _hamletText)
+        {
+            if (c == '\n')
+            {
+                indexCurrent += 1;
+                var line = _hamletText.Substring(indexPrev, indexCurrent - indexPrev);
+                if (line.Equals("\n"))
+                    rowNum++;
+                indexPrev = indexCurrent;
+                continue;
+            }
+
+            indexCurrent++;
+        }
+    }
+
+    [Benchmark]
+    public void ParseWithSpan()
+    {
+        var hamletSpan = _hamletText.AsSpan();
+        var indexPrev = 0;
+        var indexCurrent = 0;
+        var rowNum = 0;
+        foreach (var c in hamletSpan)
+        {
+            if (c == '\n')
+            {
+                indexCurrent += 1;
+                var slice = hamletSpan.Slice(indexPrev, indexCurrent - indexPrev);
+                if (slice.Equals("\n", StringComparison.OrdinalIgnoreCase))
+                    rowNum++;
+                indexPrev = indexCurrent;
+                continue;
+            }
+
+            indexCurrent++;
+        }
+    }
+
+    #endregion
+}

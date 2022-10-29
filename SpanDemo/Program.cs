4b0c47433ffb1d8bcb9001879be75c93fc2b069d
marc.ranchin <marc.ranchin@mylittlefintech.com>
2022-10-29 14:43:30 +0200

    another checkin

--- SpanDemo/Program.cs
+++ SpanDemo/Program.cs
@@ -3,6 +3,6 @@ using SpanDemo;
 
 var summary1 = BenchmarkRunner.Run<SpanWithInt>();
 
-//var summary2 = BenchmarkRunner.Run<SpanVsString>();
+var summary2 = BenchmarkRunner.Run<SpanVsString>();
 
 Console.ReadLine();

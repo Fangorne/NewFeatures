b0077f168f3a434a8ac66533f8affae09cf8a081
marc.ranchin <marc.ranchin@mylittlefintech.com>
2022-10-30 14:45:07 +0100

    refactoring

--- SpanDemo/SpanWithInt.cs
+++ SpanDemo/SpanWithInt.cs
@@ -2,61 +2,70 @@
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Jobs;
 
-namespace SpanDemo
+namespace SpanDemo;
+
+[SimpleJob(RuntimeMoniker.Net60)]
+[SimpleJob(RuntimeMoniker.Net70)]
+[RPlotExporter]
+public class SpanWithInt
 {
-    [SimpleJob(RuntimeMoniker.Net60)]
-    [SimpleJob(RuntimeMoniker.Net70)]
-    [RPlotExporter]
-    public class SpanWithInt
+    #region fields
+
+    private readonly Random _random = new(420);
+    private List<int> _items;
+
+    #endregion
+
+    #region properties
+
+    [Params(100, 100_000)] public int Size { get; set; }
+
+    #endregion
+
+    #region
+
+    [GlobalSetup]
+    public void Setup()
     {
-        [Params(100, 100_000)]
-        public int Size { get; set; }
-        private readonly Random _random = new Random(420);
-        private List<int> _items;
+        _items = Enumerable.Range(1, 100).Select(_ => _random.Next()).ToList();
+    }
 
-        [GlobalSetup]
-        public void Setup()
+    [Benchmark]
+    public void For()
+    {
+        for (var i = 0; i < _items.Count; i++)
         {
-            _items = Enumerable.Range(1, 100).Select(_ => _random.Next()).ToList();
-        }
-
-        [Benchmark]
-        public void For()
-        {
-            for (int i = 0; i < _items.Count; i++)
-            {
-                var item = _items[i];
-            }
-        }
-
-        [Benchmark]
-        public void Foreach()
-        {
-            foreach (var item1 in _items)
-            {
-                
-            }
-        }
-
-        [Benchmark]
-        public void ForeachLinq()
-        {
-            _items.ForEach(c => { });
-        }
-
-        [Benchmark]
-        public void ParallelForeachLinq()
-        {
-            Parallel.ForEach(_items, c => { });
-        }
-
-        [Benchmark]
-        public void Span()
-        {
-            foreach (var item in CollectionsMarshal.AsSpan(_items)) 
-            {
-                
-            }
+            var item = _items[i];
         }
     }
+
+    [Benchmark]
+    public void Foreach()
+    {
+        foreach (var item1 in _items)
+        {
+        }
+    }
+
+    [Benchmark]
+    public void ForeachLinq()
+    {
+        _items.ForEach(c => { });
+    }
+
+    [Benchmark]
+    public void ParallelForeachLinq()
+    {
+        Parallel.ForEach(_items, c => { });
+    }
+
+    [Benchmark]
+    public void Span()
+    {
+        foreach (var item in CollectionsMarshal.AsSpan(_items))
+        {
+        }
+    }
+
+    #endregion
 }
